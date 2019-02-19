af8dc690a6cd8f6a4751e79175ddb3804b8ee60a
chenwenhao <cwenhao88888@gmail.com>
2019-02-19 15:40:08 +0800

    innit

---
+++ pages/logs/logs.js
@@ -0,0 +1,15 @@
+//logs.js
+import { formatTime } from '../../lib/util.js'
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return formatTime(new Date(log))
+      })
+    })
+  }
+})
